5efa21260bbc5757bb2bdbf251c6b9ab0c07b3c9
Chris Slocum <Christopher.Slocum@colostate.edu>
2021-05-25 14:45:44 +0000

    Make routine into a package

---
+++ custom_colormaps/__init__.py
@@ -0,0 +1,1 @@
+from .custom_colormaps import create_colormap
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='custom_colormaps',
+    version='1.0c',
+    description='A small routine to generate custom colormaps for Matplotlib',
+    long_description=readme,
+    author='Chris Slocum',
+    author_email='Christopher.Slocum@colostate.edu',
+    url='https://github.com/CSlocumWX/custom_colormap',
+    license=license,
+    packages=find_packages(exclude=('test', 'docs'))
+)
